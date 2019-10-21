6919855821a9d5c425dde984f2e3a6f174780072
zegenerative <zegerdevos@gmail.com>
2019-10-21 14:58:18 +0200

    Add question model

---
+++ Question/model.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+// const Answer = require('../Answer/model')
+// const Category = require('../Caterogry/model')
+
+const Question = db.define('question', {
+    content: {
+        type: Sequelize.STRING
+    },
+    initialLevel: {
+        type: Sequelize.INTEGER
+    },
+    calculatedLevel: {
+        type: Sequelize.INTEGER
+    }
+})
+
+Question.hasMany(Answer)
+Category.belongsTo(Question)
+
