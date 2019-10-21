1d38b6634cc092d2ca39fabffd8bcbf8677d1a19
zegenerative <zegerdevos@gmail.com>
2019-10-21 15:35:04 +0200

    Commit before pull

--- answers/model.js
+++ answers/model.js
@@ -1,4 +1,4 @@
-const Sequelize = require('./node_modules/sequelize')
+const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Answer = db.define('answer', {
--- answers/router.js
+++ answers/router.js
@@ -1,4 +1,4 @@
-const { Router } = require('./node_modules/express')
+const { Router } = require('express')
 const Answer = require('./model')
 const router = new Router()
 
