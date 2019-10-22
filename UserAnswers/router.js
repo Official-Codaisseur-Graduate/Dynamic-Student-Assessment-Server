2689353228e1bccae37ece479aee93351d13cc72
LPascholatti <lucaspascholatti@gmail.com>
2019-10-22 11:58:42 +0200

    Add routers to endpoint userAnswers

--- UserAnswers/router.js
+++ UserAnswers/router.js
@@ -2,10 +2,35 @@ const { Router } = require('express')
 const UserAnswer = require('./model')
 const router = new Router()
 
-router.post('userAnswers', (req, res, next) => {
+router.post('/userAnswers', (req, res, next) => {
   UserAnswer
   .create(req.body)
   .then(userAnswers => res.send(userAnswers))
+  .catch(next)
+})
+
+router.get('/userAnswers', (req, res, next) => {
+  const limit = req.query.limit || 25
+  const offset = req.query.offset || 0
+
+  UserAnswer
+  .findAll({limit, offset})
+  .then(userAnswers => res.send(userAnswers))
+  .catch(next)
+})
+
+router.get('/userAnswer', (req, res, next) => {
+  UserAnswer
+  .findByPk(req.params.id)
+  .then(userAnswer => res.send(userAnswer))
+  .catch(next)
+})
+
+router.put('/userAnswer', (req, res, next) => {
+  UserAnswer
+  .findByPk(req.params.id)
+  .then(userAnswer => res.send(userAnswer))
+  .then(userAnswer => res.status(201).send(userAnswer))
   .catch(next)
 })
 
--- db.js
+++ db.js
@@ -9,7 +9,7 @@ console.log('database_url:', process.env.DATABASE_URL);
 const db = new Sequelize(databaseUrl);
 
 db
-.sync({force: true})
+.sync({force: false})
 .then(() => console.log('Database schema updated'))
 .catch(console.error)
 
