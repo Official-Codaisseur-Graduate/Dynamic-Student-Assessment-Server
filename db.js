5b050ce1487726d2cb4c8c8b50dcd1899003d6f9
zegenerative <zegerdevos@gmail.com>
2019-10-23 15:25:27 +0200

    Modify files for testing

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+
 const databaseUrl =
   process.env.DATABASE_URL ||
   'postgres://postgres:secret@localhost:5432/postgres';
@@ -6,5 +7,10 @@ const databaseUrl =
 console.log('database_url:', process.env.DATABASE_URL);
 
 const db = new Sequelize(databaseUrl);
+
+db
+  .sync({force: false})
+  .then(() => console.log('Database schema updated'))
+  .catch(console.error)
 
 module.exports = db;
