ad3c0f2a604b3d701e79965d8acf0ed08b1f3662
zegenerative <zegerdevos@gmail.com>
2019-10-24 14:37:16 +0200

    Updated version question fetching with algo

--- AdaptiveQuestionAlgorithm.js
+++ AdaptiveQuestionAlgorithm.js
@@ -1,16 +1,17 @@
-function AdaptiveQuestionAlgorithm(answer) {
+function AdaptiveQuestionAlgorithm(givenAnswer) {
     let newLevel = 0
+    if(givenAnswer !== null) {
+        if(givenAnswer.answer.correct === true) {
+            console.log('THIS ANSWER IS CORRECT')
+            newLevel += 1
+        }
 
-    if(answer.answer.correct === true) {
-        console.log('THIS ANSWER IS CORRECT')
-        newLevel += 1
+        //implement
+        //check frequency of answer (in)correct
+        //check if pattern in answering related to category
+
+        console.log('this is the new level', newLevel)
     }
-
-    //implement
-    //check frequency of answer (in)correct
-    //check if pattern in answering related to category
-
-    console.log('this is the new level', newLevel)
     return newLevel
 }
 
