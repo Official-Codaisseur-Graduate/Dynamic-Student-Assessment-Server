7dcc54d9afce1caac86254e8b1c3434d2fc6425e
zegenerative <zegerdevos@gmail.com>
2019-10-25 15:09:16 +0200

    Change put endpoint, not including dummy post anymore

--- UserAnswer/router.js
+++ UserAnswer/router.js
@@ -1,40 +1,52 @@
 const { Router } = require('express')
 const UserAnswer = require('./model')
+const Answer = require('../Answer/model')
+const Category = require('../Category/model')
+const Question = require('../Question/model')
 const router = new Router()
 
 // when getting a new question (front end), immediately create an 'empty' UserAnswer (to be updated later when actually answering the question)
-// this is so that the student can go back and change their answer
-router.post('/userAnswer', (req, res, next) => {
-  // this is the frontend way:
-  // UserAnswer.create({
-    // userId: req.body.user.userId,
-    // questionId: req.body.Question.id,
-    // categoryId: req.body.Category.id
-  // })
+// // this is so that the student can go back and change their answer
+// router.post('/userAnswer', (req, res, next) => {
+//   // this is the frontend way:
+//   // UserAnswer.create({
+//     // userId: req.body.user.userId,
+//     // questionId: req.body.Question.id,
+//     // categoryId: req.body.Category.id
+//   // })
 
-  // this is the backend testing way:
-  UserAnswer.create(req.body)
-  .then(userAnswer => {
-    res.send(userAnswer)
-  })
-  .catch(next)
-})
+//   // this is the backend testing way:
+//   UserAnswer.create(req.body)
+//   .then(userAnswer => {
+//     res.send(userAnswer)
+//   })
+//   .catch(next)
+// })
 
-// here is where we actually update the 'correct' column when the question is answered.
-router.put('/userAnswer/:id', async (req, res, next) => {
-  const chosenAnswer = await Answer.findByPk(req.params.id)
+// the req.params.id here is NOT the userAnswer id, but the id of the chosen answer in the front end
+router.put('/userAnswer/:id/:answerId', async (req, res, next) => {
+  const chosenAnswer = await Answer.findByPk(req.params.answerId)
   const correct = chosenAnswer.correct
-  const userAnswer = UserAnswer.findByPk(req.params.id)
-    // this is the frontend way:
-    // answer.update({
-    //   correct: req.body.answer.correct
-    // })
+  let userAnswer = await UserAnswer.findByPk(req.params.id)
+  
+  if(!userAnswer) {
+    userAnswer = await UserAnswer.create(
+      // {
+      // userId: req.body.user.userId,
+      // questionId: req.body.Question.id,
+      // categoryId: req.body.Category.id
+      // }
+      req.body
+    )
+  }
 
-    //this is the backend testing way:
-  const updatedAnswer = userAnswer.update({
+  const updatedUserAnswer = await userAnswer.update({
+    // categoryId: chosenAnswer.Category.id,
+    // questionId: chosenAnswer.Question.id,
+    answerId: req.params.answerId,
     correct: correct
   })
-    res.send(updatedAnswer)
+    res.send(updatedUserAnswer)
 })
 
 //get all userAnswers of every user
