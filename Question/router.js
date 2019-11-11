63ce5dcdea61dc9995bfafed97b03e6bc726ca20
qiyu1987 <qiyu1987@gmail.com>
2019-11-11 18:19:53 +0100

    get random question

--- Question/router.js
+++ Question/router.js
@@ -1,149 +1,149 @@
-const { Router } = require('express')
-const Question = require('./model')
-const Answer = require('../Answer/model')
-const UserAnswer = require('../UserAnswer/model')
-const Category = require('../Category/model')
+const { Router } = require("express")
+const Question = require("./model")
+const Answer = require("../Answer/model")
+const UserAnswer = require("../UserAnswer/model")
+const Category = require("../Category/model")
 const router = new Router()
-const AdaptiveQuestionAlgorithm = require('../AdaptiveQuestionAlgorithm')
+const AdaptiveQuestionAlgorithm = require("../AdaptiveQuestionAlgorithm")
 
-router.post('/question', async (req, res, next) => {
-    const { questionContent, categoryId } = req.body
- 
-    if (questionContent && categoryId) {
-         const newQuestion = {
-            questionContent,
-             initialLevel: null,
-            calculatedLevel: null,
-            categoryId
-        }
+router.post("/question", async (req, res, next) => {
+	const { questionContent, categoryId } = req.body
 
-        await Question.create(newQuestion)
-        .then(result => res.status(201).json(result.id))
-        .catch(error => console.log('Error while creating new question: ', error))
-    } else {
-        res.status(400).send({'message' : 'Please complete all the required fields'})
-    }
- })
+	if (questionContent && categoryId) {
+		const newQuestion = {
+			questionContent,
+			initialLevel: null,
+			calculatedLevel: null,
+			categoryId
+		}
 
-router.get('/question', (req, res, next) => {
-  //intial setup for pagination?
-  const limit = req.query.limit || 25
-  const offset = req.query.offset || 0
-  Question
-      .findAll({
-            limit,
-            offset,
-            include: [
-                {
-                    model: Category,
-                    attributes: ['topic']
-                },
-                {
-                    model: Answer,
-                },
-            ]
-        })
-      .then(questions => {
-          if(!questions) {
-              res.status(404).send('No questions found')
-          } else {
-              return res.send(questions)
-          }
-    })
-  .catch(next)
+		await Question.create(newQuestion)
+			.then(result => res.status(201).json(result.id))
+			.catch(error => console.log("Error while creating new question: ", error))
+	} else {
+		res.status(400).send({ message: "Please complete all the required fields" })
+	}
 })
 
-router.get('/question/:index', async (req, res, next) => {
-    try { 
-        let newLevel = 0
-
-        const newQuestions = await Question.findAll({ 
-            where: { 
-                initialLevel: 0,
-            },
-            include: [
-                {
-                    model: Category,
-                    attributes: ['topic']
-                },
-                {
-                    model: Answer,
-                }
-            ]
-        })
-        let randomFirst  = Math.floor(Math.random() * Math.floor(newQuestions.length))
-        const firstQuestion = newQuestions[randomFirst]
-
-        const previousAnswer = await UserAnswer.findOne({     
-            where: {
-                questionId: req.params.index - 1
-            },
-            include: [{
-                model: Answer,
-                attributes: ['correct']
-            }]
-        })
-    
-        //put that previous answer in the algorithm and check if it was correct
-        //(it returns a newLevel)
-        newLevel = await AdaptiveQuestionAlgorithm(previousAnswer)
-        
-        //lastly, return a new question, based on the level the algortithm decides
-        const possibleNewQuestions = await Question.findAll({ 
-            where: { 
-                initialLevel: newLevel,
-            },
-            include: [
-                {
-                    model: Category,
-                    attributes: ['topic']
-                },
-                {
-                    model: Answer
-                },
-            ]
-        })
-        
-        let randomNew  = Math.floor(Math.random() * Math.floor(possibleNewQuestions.length))
-        const newQuestion = possibleNewQuestions[randomNew]
-
-        //if this is the very first question of the test
-        if(req.params.index !== 1) {
-            res.send(newQuestion) 
-        } else {
-            res.send(firstQuestion)
-        }
-    }
-    catch(error) {
-        console.error(error)
-    }
+router.get("/question", (req, res, next) => {
+	//intial setup for pagination?
+	const limit = req.query.limit || 25
+	const offset = req.query.offset || 0
+	Question.findAll({
+		limit,
+		offset,
+		include: [
+			{
+				model: Category,
+				attributes: ["topic"]
+			},
+			{
+				model: Answer
+			}
+		]
+	})
+		.then(questions => {
+			if (!questions) {
+				res.status(404).send("No questions found")
+			} else {
+				return res.send(questions)
+			}
+		})
+		.catch(next)
 })
 
-router.put('/question/:id', (req, res, next) => {
-  Question.findByPk(req.params.id)
-  .then(question => {
-      if(!question) {
-          res.status(404).send('question not found')
-      } else {
-          question.update(req.body)
-          .then(updatedQuestion => {
-              res.send(updatedQuestion)
-          })
-      }
-  })
-  .catch(next)
+router.get("/question/:index", async (req, res, next) => {
+	try {
+		let newLevel = 0
+
+		const newQuestions = await Question.findAll({
+			where: {
+				initialLevel: 0
+			},
+			include: [
+				{
+					model: Category,
+					attributes: ["topic"]
+				},
+				{
+					model: Answer
+				}
+			]
+		})
+		let randomFirst = Math.floor(
+			Math.random() * Math.floor(newQuestions.length)
+		)
+		const firstQuestion = newQuestions[randomFirst]
+
+		const previousAnswer = await UserAnswer.findOne({
+			include: [
+				{
+					model: Answer,
+					attributes: ["correct", "questionId"],
+					where: { questionId: req.params.index - 1 }
+				}
+			]
+		})
+
+		//put that previous answer in the algorithm and check if it was correct
+		//(it returns a newLevel)
+		newLevel = await AdaptiveQuestionAlgorithm(previousAnswer)
+
+		//lastly, return a new question, based on the level the algortithm decides
+		const possibleNewQuestions = await Question.findAll({
+			where: {
+				initialLevel: newLevel
+			},
+			include: [
+				{
+					model: Category,
+					attributes: ["topic"]
+				},
+				{
+					model: Answer
+				}
+			]
+		})
+
+		let randomNew = Math.floor(
+			Math.random() * Math.floor(possibleNewQuestions.length)
+		)
+		const newQuestion = possibleNewQuestions[randomNew]
+
+		//if this is the very first question of the test
+		if (req.params.index !== 1) {
+			res.send(newQuestion)
+		} else {
+			res.send(firstQuestion)
+		}
+	} catch (error) {
+		console.error(error)
+	}
 })
 
-router.delete('/question/:id', (req, res, next) => {
-  Question.findByPk(req.params.id)
-  .then(question => {
-      if(!question) {
-          res.status(404).send('question not found')
-      } else {
-          question.destroy()
-          res.status(201).send(`Destroyed question ${req.params.id}`)
-      }
-  })
+router.put("/question/:id", (req, res, next) => {
+	Question.findByPk(req.params.id)
+		.then(question => {
+			if (!question) {
+				res.status(404).send("question not found")
+			} else {
+				question.update(req.body).then(updatedQuestion => {
+					res.send(updatedQuestion)
+				})
+			}
+		})
+		.catch(next)
 })
 
-module.exports = router;
+router.delete("/question/:id", (req, res, next) => {
+	Question.findByPk(req.params.id).then(question => {
+		if (!question) {
+			res.status(404).send("question not found")
+		} else {
+			question.destroy()
+			res.status(200).send(`Destroyed question ${req.params.id}`)
+		}
+	})
+})
+
+module.exports = router
