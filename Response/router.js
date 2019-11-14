70df84edb4f2525759e8176bc1124ad08389cab5
qiyu1987 <qiyu1987@gmail.com>
2019-11-14 13:39:14 +0100

    combined to one response endpoint

--- Response/router.js
+++ Response/router.js
@@ -1,68 +1,59 @@
 const { Router } = require("express")
 const Answer = require("../Answer/model")
 const Test = require("../Test/model")
+const Question = require("../Question/model")
 const router = new Router()
+const { maxDifficultyLevel } = require("../constants")
+const Sequelize = require("sequelize")
+const Op = Sequelize.Op
 
 // when interviewee selected an answer for a test
 // send http put "baseUrl/response?testId=id&answerId=id"
 // it is going to find all the answers in the test,
 //	-if there is already an answer to the same question, replace the answer
 //  -else add the answer
-router.put("/response", async (req, res, next) => {
+// after add the answer, response with a new question, that is of the new level and
+// is not a question already in the test
+router.post("/response", async (req, res, next) => {
 	try {
 		const { testId, answerId } = req.query
 		// find all the answers in the test
 		const test = await Test.findByPk(testId)
 		const answers = await test.getAnswers()
-		// find the new answer provided
-		const newAnswer = await Answer.findByPk(answerId)
-		// there is an old answer to the same question
-		const oldAnswer = answers.filter(
-			answer => answer.questionId === newAnswer.questionId
-		)
-		// remove the oldAnswer
-		if (oldAnswer) await test.removeAnswer(oldAnswer)
-		// add the newAnswer
-		await test.addAnswer(newAnswer)
-		res.send(test)
-	} catch (error) {
-		next(error)
-	}
-})
-
-// when user taking testId need a new question
-// send request with query params of testId and previousAnswerId
-// front end make a request as "baseurl/testquestion?testId=id&previousAnswerId=id"
-router.get("/test-question", async (req, res, next) => {
-	try {
-		// get previousAnswerId  from request query params
-		const { previousAnswerId, testId } = req.query
-		// find the previous answer and question
-		const previousAnswer = await Answer.findByPk(previousAnswerId, {
+		// find the current answer provided
+		const currentAnswer = await Answer.findByPk(answerId, {
 			include: [Question]
 		})
-		// if there is no previous Answer, there is no previous Question
-		// then it is the first question
 
-		const previousQuestion = !previousAnswer ? null : previousAnswer.question
-		const correct = !previousAnswer ? false : previousAnswer.correct
-		// for first question, correct = false, previousLevel = 0
-		// get previousLevel and correctness and calculate level
-		const previousLevel = !previousQuestion ? 0 : previousQuestion.initialLevel
-		const level =
-			!correct || previousLevel === maxDifficultyLevel
-				? Number(previousLevel)
-				: Number(previousLevel) + 1
+		// if currentAnswer does not exist, this is the first attempt to get a question
+		// there is an old answer to the same question
+		// if this is the question without an answer oldAnswer=null
+		const oldAnswer = !currentAnswer
+			? null
+			: answers.filter(answer => answer.questionId === currentAnswer.questionId)
+		// remove the oldAnswer
+		if (oldAnswer) await test.removeAnswer(oldAnswer)
+		// add the currentAnswer
+		await test.addAnswer(currentAnswer)
+		// if there is no current Answer, there is no current Question
+		// then it is the first question
+		const currentQuestion = !currentAnswer ? null : currentAnswer.question
+		// for first question, correct = false, currentLevel = 0
+		const correct = !currentAnswer ? false : currentAnswer.correct
+		// get currentLevel and correctness and calculate level
+		const currentLevel = !currentQuestion ? 0 : currentQuestion.initialLevel
+		const nextLevel =
+			!correct || currentLevel === maxDifficultyLevel
+				? Number(currentLevel)
+				: Number(currentLevel) + 1
 		// find questionIds of answers already in the test
-		const test = await Test.findByPk(testId)
-		const answers = await test.getAnswers()
 		const questionIds = answers.map(answer => answer.questionId)
 		// find all questions of newLevel excluding the ones already in questionIds
 
 		const questions = await Question.findAll({
 			// when there are test model as well, you can exclude questions already in the test
 			where: {
-				initialLevel: level,
+				initialLevel: nextLevel,
 				id: { [Op.notIn]: questionIds }
 			}
 		})
