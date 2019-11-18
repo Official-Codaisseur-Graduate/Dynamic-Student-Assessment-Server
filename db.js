5d46de3aa5fa7df512a4f889e77b28130259fca9
qiyu1987 <qiyu1987@gmail.com>
2019-11-18 12:13:43 +0100

    clean up db file

--- db.js
+++ db.js
@@ -4,12 +4,5 @@ const databaseUrl =
 	process.env.DATABASE_URL ||
 	"postgres://postgres:secret@localhost:5432/postgres"
 
-console.log("database_url:", databaseUrl)
-
 const db = new Sequelize(databaseUrl)
-
-// db.sync({ force: false })
-// 	.then(() => console.log("Database schema updated"))
-// 	.catch(console.error)
-
 module.exports = db
