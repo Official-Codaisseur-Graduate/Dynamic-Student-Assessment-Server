6927534ae8d88599d08626c040bba06120c04fc8
vhs2708 <veronicahvaalstigen@me.com>
2019-12-02 17:42:41 +0100

    10 more questions and email fix

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const app = require("./app")
-const port = process.env.PORT || 4000
+const app = require("./app");
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-	console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
 
-const db = require("./db")
-const Question = require("./Question/model")
-const Answer = require("./Answer/model")
-const Category = require("./Category/model")
-const Interviewee = require("./Interviewee/model")
-const Admin = require("./Admin/model")
-const Test = require("./Test/model")
-const Response = require("./Response/model")
+const db = require("./db");
+const Question = require("./Question/model");
+const Answer = require("./Answer/model");
+const Category = require("./Category/model");
+const Interviewee = require("./Interviewee/model");
+const Admin = require("./Admin/model");
+const Test = require("./Test/model");
+const Response = require("./Response/model");
 
 const {
-	adminListHashedPassword,
-	intervieweeList,
-	categoryList,
-	questionList,
-	answerList,
-	testList,
-	responseList
-} = require("./seeding")
+  adminListHashedPassword,
+  intervieweeList,
+  categoryList,
+  questionList,
+  answerList,
+  testList,
+  responseList
+} = require("./seeding");
 
-db.sync({ force: true })
-	.then(async () => {
-		console.log("Database schema updated")
+db.sync({ force: false })
+  /*.then(async () => {
+    console.log("Database schema updated");
 
-		await Admin.bulkCreate(adminListHashedPassword)
-		await Interviewee.bulkCreate(intervieweeList)
-		await Category.bulkCreate(categoryList)
-		await Question.bulkCreate(questionList)
-		await Answer.bulkCreate(answerList)
-		await Test.bulkCreate(testList)
-		await Response.bulkCreate(responseList)
-	})
-	.catch(console.error)
+    await Admin.bulkCreate(adminListHashedPassword);
+    await Interviewee.bulkCreate(intervieweeList);
+    await Category.bulkCreate(categoryList);
+    await Question.bulkCreate(questionList);
+    await Answer.bulkCreate(answerList);
+    await Test.bulkCreate(testList);
+    await Response.bulkCreate(responseList);
+  })*/
+  .catch(console.error);
