a729dcc3f49273f3bc61f16b45e9d960eb9c2498
adeltancsik <tancsikadel@gmail.com>
2019-12-03 13:33:05 +0100

    Modified edit question endpoint

--- Question/router.js
+++ Question/router.js
@@ -1,86 +1,99 @@
-const { Router } = require("express")
-const Question = require("./model")
-const Answer = require("../Answer/model")
-const Category = require("../Category/model")
-const router = new Router()
-const auth = require("../Auth/middleware")
+const { Router } = require("express");
+const Question = require("./model");
+const Answer = require("../Answer/model");
+const Category = require("../Category/model");
+const router = new Router();
+const auth = require("../Auth/middleware");
 
 // create a new question
 router.post("/question", auth, async (req, res, next) => {
-	try {
-		const { questionContent, categoryId, level } = req.body
+  try {
+    const { questionContent, categoryId, level } = req.body;
 
-		if (questionContent && categoryId) {
-			const newQuestion = {
-				questionContent,
-				initialLevel: level,
-				categoryId
-			}
+    if (questionContent && categoryId) {
+      const newQuestion = {
+        questionContent,
+        initialLevel: level,
+        categoryId
+      };
 
-			const question = await Question.create(newQuestion)
-			if (!question) res.status(400).end()
-			res.status(201).send(question)
-		} else {
-			res
-				.status(400)
-				.send({ message: "Please complete all the required fields" })
-		}
-	} catch (error) {
-		next(error)
-	}
-})
+      const question = await Question.create(newQuestion);
+      if (!question) res.status(400).end();
+      res.status(201).send(question);
+    } else {
+      res
+        .status(400)
+        .send({ message: "Please complete all the required fields" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 // get all the questions
 router.get("/question", auth, (req, res, next) => {
-	const limit = req.query.limit || 25
-	const offset = req.query.offset || 0
-	Question.findAll({
-		limit,
-		offset,
-		include: [
-			{
-				model: Category,
-				attributes: ["topic"]
-			},
-			{
-				model: Answer
-			}
-		]
-	})
-		.then(questions => {
-			if (!questions) {
-				res.status(404).send("No questions found")
-			} else {
-				return res.send(questions)
-			}
-		})
-		.catch(next)
-})
+  const limit = req.query.limit || 25;
+  const offset = req.query.offset || 0;
+  Question.findAll({
+    limit,
+    offset,
+    include: [
+      {
+        model: Category,
+        attributes: ["topic"]
+      },
+      {
+        model: Answer
+      }
+    ]
+  })
+    .then(questions => {
+      if (!questions) {
+        res.status(404).send("No questions found");
+      } else {
+        return res.send(questions);
+      }
+    })
+    .catch(next);
+});
 //Edit a question
 router.put("/question/:id", (req, res, next) => {
-	Question.findByPk(req.params.id)
-		.then(question => {
-			if (!question) {
-				res.status(404).send("question not found")
-			} else {
-				question.update(req.body).then(updatedQuestion => {
-					res.send(updatedQuestion)
-				})
-			}
-		})
-		.catch(next)
-})
+  const { questionContent, categoryId, level } = req.body;
+  if (questionContent && categoryId) {
+    const editedQuestion = {
+      questionContent,
+      initialLevel: level,
+      categoryId
+    };
+    Question.findByPk(req.params.id)
+      .then(question => {
+        if (!question) {
+          res.status(404).send("question not found");
+        } else {
+          question.update(editedQuestion).then(updatedQuestion => {
+            res.send(updatedQuestion);
+          });
+        }
+      })
+      .catch(next);
+  } else {
+    res
+      .status(400)
+      .send({ message: "Please complete all the required fields" });
+  }
+});
+
 //Delete a question
 router.delete("/question/:id", (req, res, next) => {
-	Question.findByPk(req.params.id)
-		.then(question => {
-			if (!question) {
-				res.status(404).send("question not found")
-			} else {
-				question.destroy()
-				res.status(200).send(`Destroyed question ${req.params.id}`)
-			}
-		})
-		.catch(next)
-})
+  Question.findByPk(req.params.id)
+    .then(question => {
+      if (!question) {
+        res.status(404).send("question not found");
+      } else {
+        question.destroy();
+        res.status(200).send(`Destroyed question ${req.params.id}`);
+      }
+    })
+    .catch(next);
+});
 
-module.exports = router
+module.exports = router;
