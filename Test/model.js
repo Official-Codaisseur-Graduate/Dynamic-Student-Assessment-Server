74e9690400e39d2a1bd4c9588a29e40da6af1669
mtabanja <mouaztabanja@outlook.com>
2019-12-05 12:43:00 +0100

    put request for expire the code

--- Test/model.js
+++ Test/model.js
@@ -1,20 +1,20 @@
-const Sequelize = require("sequelize")
-const db = require("../db")
-const Interviewee = require("../Interviewee/model")
+const Sequelize = require("sequelize");
+const db = require("../db");
+const Interviewee = require("../Interviewee/model");
 
 const Test = db.define("test", {
-	score: {
-		type: Sequelize.INTEGER
-	},
-	status: {
-		type: Sequelize.INTEGER
-	},
-	code: {
-		type: Sequelize.STRING
-	}
-})
+  score: {
+    type: Sequelize.INTEGER
+  },
+  status: {
+    type: Sequelize.BOOLEAN
+  },
+  code: {
+    type: Sequelize.STRING
+  }
+});
 
-Test.belongsTo(Interviewee)
-Interviewee.hasMany(Test)
+Test.belongsTo(Interviewee);
+Interviewee.hasMany(Test);
 
-module.exports = Test
+module.exports = Test;
--- index.js
+++ index.js
@@ -26,13 +26,13 @@ const {
 
 db.sync({ force: false })
   .then(async () => {
-    console.log("Database schema updated");
-    /* await Admin.bulkCreate(adminListHashedPassword);
-    await Interviewee.bulkCreate(intervieweeList);
-    await Category.bulkCreate(categoryList);
-    await Question.bulkCreate(questionList);
-    await Answer.bulkCreate(answerList);
-    await Test.bulkCreate(testList);
-    await Response.bulkCreate(responseList); */
+    // console.log("Database schema updated");
+    // await Admin.bulkCreate(adminListHashedPassword);
+    // await Interviewee.bulkCreate(intervieweeList);
+    // await Category.bulkCreate(categoryList);
+    // await Question.bulkCreate(questionList);
+    // await Answer.bulkCreate(answerList);
+    // await Test.bulkCreate(testList);
+    // await Response.bulkCreate(responseList);
   })
   .catch(console.error);
